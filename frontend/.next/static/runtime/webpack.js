89d47e8bfb501c65e7c6d9873949ef041b7324ef
luis <luis.scholars@gmail.com>
2019-01-27 18:59:08 -0500

    commit of the day

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "efb9cfda240063eb838e";
+/******/ 	var hotCurrentHash = "892224ec988817836537";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
